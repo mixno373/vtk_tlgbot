7657497cb45684df0a29611ad7d5ba915bdfccd8
Michael <mixno20@gmail.com>
2024-01-16 23:24:11 +0800

    Стартовая точка
    Старт работы над проектом.
    Прибрался в файлах, убрал лишнее, поставил в игнор не относящиеся файлы

---
+++ conf/const.py
@@ -0,0 +1,199 @@
+import aiohttp, random
+import re
+from datetime import datetime
+
+from telegram_bot_calendar import DetailedTelegramCalendar
+
+from PIL import Image
+
+
+
+random.seed()
+
+APP_LATEST_VERSION = "1.17.2-81"
+APP_LATEST_UPDATE_URI = f"/home/ubuntu/bots/lasertag/apk/Diverstat_{APP_LATEST_VERSION}.apk"
+APP_STALKER_URI = f"/home/ubuntu/bots/lasertag/apk/Diverstat - Stalker v1.18.0-90.apk"
+
+QRCODES_SAVE_PATH = "statistic/qrcodes"
+QRCODES_FILENAME = "{uid}_{lang}_{theme_name}"
+BEST_PLAYERS_SAVE_PATH = "statistic/best_players"
+BEST_PLAYERS_FILENAME = "Best_Players_{polygon_uid}_{lang}_{sort}_{theme_name}_{limit}_{minimum_games_played}"
+
+VTK_TLG_LT_LOGCHAT_ID = -4059829948
+
+HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+
+admins_list = [
+    638308159215370252,
+    554418178940338194,
+    659257029172396052,
+    650258096098377767,
+
+]
+
+logos_list = {
+    638308159215370252: "vtk",
+    829769846106488922: "vtk",
+    34: "phoenix",
+    32: "druzhina",
+    757506057110159390: "druzhina",
+    650258096098377767: "vtk",
+    554418178940338194: "vtk",
+    659257029172396052: "vtk",
+    1: "vtk",
+    37: "vtk",
+    38: "vtk",
+    47: "vtk",
+    688289274151436308: "patriots",
+    640160464181133323: "vtk_zima",
+    49: "vtk_zima",
+    692625321219850311: "bryansk",
+    124: "fine_br",
+    154: "spetz",
+    297: "br",
+    298: "sarmat",
+    319: "vl",
+    302: "stalker",
+    175: "default",
+}
+
+vtk_users = [
+    638308159215370252,
+    650258096098377767,
+    554418178940338194,
+    659257029172396052,
+    829769846106488922,
+    1
+]
+
+DEFAULT_REVIEWS_CHAT_IDS = {
+    "vl": "-1001501905733"
+}
+
+RALC_POLYGON_UID = 4
+
+
+TELEGRAM_CHANNELS = {
+    "vtk": "-1001189271225",
+    "br": "-1001877745134",
+    "vl": "-1001501905733"
+}
+
+def unix_time():
+    return int(datetime.utcnow().timestamp())
+
+
+def split_int(value, char = "."):
+    char = str(char)
+    value = str(value)
+    pattern = "([0-9]{3})"
+    value = value[::-1]
+    value = re.sub(pattern, r"\1"+char, value)
+    value = value[::-1]
+    value = value.lstrip(char)
+    return value
+
+def get_int_from_data(data, key, min=1, max=1, default=1):
+    try:
+        value = int(data[key])
+    except:
+        value = default
+    if value > max:
+        value = max
+    if value < min:
+        value = min
+    return value
+
+
+
+def is_admin(user):
+    try:
+        if user.id in admins_list:
+            return True
+    except:
+        pass
+    return False
+
+async def say_log(text, description=None, **kwargs):
+    url = wh_log_urls.get(kwargs.pop("url", "stats"), wh_log_urls["stats"])
+    try:
+        kwargs.pop("url")
+    except:
+        pass
+
+    em = Embed(title=text)
+    if description:
+        em.description = str(description)
+    for key in sorted(kwargs.keys()):
+        value = kwargs[key]
+        em.add_field(
+            name=str(key),
+            value=str(value),
+            inline=True
+        )
+
+    em.timestamp = datetime.utcnow()
+
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url(url, adapter=AsyncWebhookAdapter(session))
+        try:
+            await webhook.send(embed=em)
+        except Exception as e:
+            print(f"webhook: Unknown exception ({e})]")
+
+
+
+
+BADGES_PER_LINE_1 = 5
+BADGES_PER_LINE_2 = 4
+PLAYERS_PER_PAGE = 19
+BEST_PLAYERS_PER_PAGE = 20
+
+
+PLOT_TIMEZONES_COUNT = 25
+PLOT_YAXIS_SCALE = 30
+
+def get_ranges(start, stop, zones):
+    zone_length = int((stop - start) / zones)
+
+    ranges = []
+
+    for i in range(zones):
+        range_ = [start+i*zone_length, start+(i+1)*zone_length-1]
+        if stop - zone_length < range_[1]:
+            range_[1] = stop
+        ranges.append(range_)
+
+    return ranges
+
+
+
+
+class MyTranslationCalendar(DetailedTelegramCalendar):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.locale = "ru"
+        
+        
+invite_backs = {
+        "vtk": Image.open("conf/img/invite_back_vtk.png"),
+        "br": Image.open("conf/img/invite_back_br.png"),
+    }
+invite_front = Image.open("conf/img/invite_front.png")
+
+MONTHS = [
+    "Января",
+    "Февраля",
+    "Марта",
+    "Апреля",
+    "Мая",
+    "Июня",
+    "Июля",
+    "Августа",
+    "Сентября",
+    "Октября",
+    "Ноября",
+    "Декабря",
+]
+
+TEXT_COLOR = (0, 0, 0)
---
+++ vtk_tlgbot.py
@@ -0,0 +1,171 @@
+import asyncio, requests
+
+import telebot
+from telebot.async_telebot import AsyncTeleBot
+from telebot import types
+
+from telegram_bot_calendar import LSTEP_tr
+
+from PIL import Image
+from io import BytesIO
+
+from conf.const import *
+from conf.tlg_invite import *
+from config.settings import settings
+
+
+
+bot = AsyncTeleBot(settings["vtk_tlg_token"], parse_mode=None)
+
+
+@bot.message_handler(content_types=['new_chat_members'])
+async def welcome_new_member(message):
+    if message.json['new_chat_participant']['id'] == 1603029657:
+        await bot.send_message(-674296167, f'Меня добавили в группу "{message.chat.title}" (ID: {message.chat.id})')
+        
+
+@bot.message_handler(content_types=['photo'])
+async def handle_photo(message):
+    if message.chat.type != "private":
+        return
+    
+    photo_size = message.photo[-1]
+    file_info = await bot.get_file(photo_size.file_id)
+    downloaded_file = await bot.download_file(file_info.file_path)
+
+    avatar = Image.open(BytesIO(downloaded_file))
+    under = Image.open("assets/logo_cs.png")
+
+    width, height = avatar.size   # Get dimensions
+    new_width = min(width, height)
+    new_height = new_width
+    left = (width - new_width)/2
+    top = (height - new_height)/2
+    right = (width + new_width)/2
+    bottom = (height + new_height)/2
+
+    # Crop the center of the image
+    avatar = avatar.crop((left, top, right, bottom))
+    under = under.resize(avatar.size)
+
+    avatar.paste(under, (0, 0), under)
+
+    await bot.send_photo(message.chat.id, avatar)
+
+
+# Handler for .json statistics
+@bot.message_handler(content_types=['document'])
+async def handle_document(message):
+    if message.chat.type != "private":
+        return
+
+    try:
+        file_info = await bot.get_file(message.document.file_id)
+        downloaded_file = await bot.download_file(file_info.file_path)
+
+        await bot.send_message(message.chat.id, f'Начата обработка файла "{message.document.file_name}".')
+
+        filename = f"{message.document.file_unique_id}"
+        with open("./statistic/"+filename+".json", "wb") as f:
+            f.write(downloaded_file)
+
+        text = message.caption.split(" ")
+  
+        try:
+            login = text[0]
+            password = text[1]
+
+            if not login: login = "-"
+            if not password: password = "-"
+        except Exception as e:
+            login = "-"
+            password = "-"
+   
+        try:
+            theme = text[2]
+
+            if not theme: theme = "dark"
+        except Exception as e:
+            theme = "dark"
+
+        s = requests.Session()
+        payload = {
+            "login": login,
+            "password": password,
+            "filename": filename,
+            "theme": theme
+        }
+        response = s.post("http://localhost:8081/api/handle_game_statistic", params=payload)
+
+        if response.status_code == 200:
+            await bot.send_message(message.chat.id, response.text)
+        elif response.status_code == 400:
+            await bot.send_message(message.chat.id, f'Не удалось обработать статистику. {response.text} ¯\_(ツ)_/¯')
+        else:
+            await bot.send_message(message.chat.id, 'Не удалось обработать статистику. Попробуйте еще раз ¯\_(ツ)_/¯')
+    except Exception as e:
+        print(e)
+
+
+@bot.message_handler(commands=['start'])
+async def command_start(message):
+    if message.chat.type == "private":
+        send_mess = f"Привет, отправь команду /invite для создания пригласительных ко дню рождения :)"
+        await bot.send_message(message.chat.id, send_mess)
+  
+@bot.message_handler(commands=['invite'])
+async def command_invite(message):
+    if message.chat.type == "private":
+          markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+          btn1 = types.KeyboardButton("Ангарск")
+          btn2 = types.KeyboardButton("Братск")
+          markup.add(btn1, btn2)
+          await bot.send_message(message.chat.id, text="Здравствуйте, для начала выберите свой город", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=MyTranslationCalendar.func())
+async def cal(c):
+    result, key, step = MyTranslationCalendar().process(c.data)
+    if not result and key:
+        await bot.edit_message_text(f"Выберите {LSTEP_tr[step]}",
+                              c.message.chat.id,
+                              c.message.message_id,
+                              reply_markup=key)
+    elif result:
+        return await makeinvite_3_date(bot, c.message, result)
+        
+        
+
+@bot.message_handler(content_types=['text'])
+async def func(message):
+    if message.chat.type != "private":
+        return
+    
+    status = False
+    
+    if message.text.lower() == "invite":
+        return await command_invite(message)
+    
+    if message.text == "Ангарск":
+        return await makeinvite_1_city(bot, message, "Ангарск", 'vtk')
+    elif message.text == "Братск":
+        return await makeinvite_1_city(bot, message, "Братск", 'br')
+    else:
+        status = get_invuser_status(message.chat.id)
+        print(f"Status: {status}")
+        
+    if status and status != "complete":
+        if status == "imeninnik_name":
+            return await makeinvite_2_imeninnikname(bot, message)
+        if status == "date":
+            calendar, step = MyTranslationCalendar().build()
+            bot.send_message(message.chat.id,
+                            f"Выберите {LSTEP_tr[step]}",
+                            reply_markup=calendar)
+        if status == "minutes":
+            return await makeinvite_3_minutes(bot, message)
+        if status == "name":
+            return await makeinvite_4_image(bot, message)
+        
+
+asyncio.run(bot.polling(timeout=300, request_timeout=300, none_stop=True))
